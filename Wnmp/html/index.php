5c1b8e66b238617e5ef45f31857231368f62b826
刘群 <517067180@qq.com>
2020-08-27 09:56:18 +0800

    index.php: prefer a relative path instead of the absolute path
    For example when nginx is configured to listen on port 8080 instead of
    port 80, the relative path in index.php would work better.

--- Wnmp/html/index.php
+++ Wnmp/html/index.php
@@ -5,7 +5,7 @@
 <body>
 <center><h2>Welcome to Wnmp 3.2.2!</h2></center>
 <center><h2><a href="https://wnmp.x64architecture.com/">Wnmp Website</a></h2></center>
-<center>Click <a href="http://localhost/phpmyadmin/">here</a> to go to phpMyAdmin</center>
+<center>Click <a href="/phpmyadmin/">here</a> to go to phpMyAdmin login page of the local host </center>
 <br />
 <?php 
 phpinfo();
